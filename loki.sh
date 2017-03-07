bd1dd0296e404b40380ba7c929f8578abd25d70d
Jung-Kyu Park <bagjunggyu@gmail.com>
2017-03-08 03:29:15 +0900

    Update loki.sh

--- loki.sh
+++ loki.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 sudo sed -i 's/loki/xenial/g' /etc/apt/sources.list.d/elementary.list /etc/apt/sources.list.d/patches.list
 sudo apt-get update
-sudo apt-get install nimf git software-properties-common gdebi -y
+sudo apt-get install nimf git software-properties-common gdebi gparted gnome-disk-utility -y
 im-config -n nimf
 gsettings set org.gnome.settings-daemon.plugins.media-keys email "'F4'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys next "'F10'"
